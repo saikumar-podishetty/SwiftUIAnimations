806bfca04abfae935c010c9cb33422f47331401e
sai kumar <sai.podishetty@dazn.com>
2023-07-06 12:48:08 +0530

    Project created
    Project created

---
+++ SwiftUIAnimations/SwiftUIAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationsApp.swift
+//  SwiftUIAnimations
+//
+//  Created by Sai Podishetty on 7/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
